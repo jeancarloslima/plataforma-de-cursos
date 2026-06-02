d2f311dd4f1609e786feae9bcee39d8f71152b1a
Jean Lima <jcoliveiralima19@gmail.com>
2026-06-02 16:29:15 -0300

    Definindo estrutura básica do rodapé

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,7 +1,39 @@
+import { Box, List, ListItem, Typography } from "@mui/material";
+
 export default function Footer() {
   return (
     <>
-      <h1>Testi</h1>
+      <Box>
+        <Typography variant="h5">Rawi</Typography>
+        <Typography variant="body1">
+          Explore um método transformativo de desenvolver habilidades na nossa
+          plataforma de ensino online.
+        </Typography>
+      </Box>
+
+      <Box>
+        <Box>
+          <List>
+            <ListItem></ListItem>
+          </List>
+        </Box>
+        <Box>
+          <List>
+            <ListItem></ListItem>
+          </List>
+        </Box>
+        <Box>
+          <List>
+            <ListItem></ListItem>
+          </List>
+        </Box>
+      </Box>
+
+      <Box>
+        <List>
+            <ListItem></ListItem>
+          </List>
+      </Box>
     </>
   );
 }
