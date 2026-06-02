600da2a6e1e2dacb1ed249c4206a4e25aeb8dc40
Jean Lima <jcoliveiralima19@gmail.com>
2026-06-02 16:21:19 -0300

    Definindo estrutura básica da página de Cursos Populares

--- src/Components/PopularCoursesLandingPage/index.jsx
+++ src/Components/PopularCoursesLandingPage/index.jsx
@@ -1,7 +1,18 @@
+import { List, ListItem, Typography } from "@mui/material";
+
 export default function PopularCoursesLandingPage() {
-    return (
-        <>
-        <h1>Testi</h1>
-        </>
-    )
+  return (
+    <>
+      <Typography variant="h5">Nossos cursos populares</Typography>
+      <Typography variant="body1">
+        Descubra nossos cursos mais procurados, curados de forma cuidadosa para
+        atender as demandas do mercado atual. Mergulhe em conteúdos construídos
+        para o sucesso em cada etapa da sua jornada de estudos.
+      </Typography>
+      <List>
+        <ListItem>
+        </ListItem>
+      </List>
+    </>
+  );
 }
