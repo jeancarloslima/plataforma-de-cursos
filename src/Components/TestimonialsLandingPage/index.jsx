fd1bf39f0d1115f1d564b35d7d58a6f648c8ca41
Jean Lima <jcoliveiralima19@gmail.com>
2026-06-02 16:24:04 -0300

    Definindo estrutura básica da página de Testemunhos

--- src/Components/TestimonialsLandingPage/index.jsx
+++ src/Components/TestimonialsLandingPage/index.jsx
@@ -1,7 +1,20 @@
+import { List, ListItem, Typography } from "@mui/material";
+
 export default function TestimonialsLandingPage() {
   return (
     <>
-      <h1>Testi</h1>
+      <Typography variant="h5">Testemunhos de Estudantes</Typography>
+      <Typography variant="body1">
+        Aqui está o que nossos estudante tem a dizer sobre suas experiências
+        tranformativas de aprendizado. Histórias reais, crescimentos reais.
+        Descubra em primeira mão o impacto que nossos cursos tiveram em suas
+        vidas.
+      </Typography>
+      <List>
+        <ListItem>
+          
+        </ListItem>
+      </List>
     </>
   );
 }
