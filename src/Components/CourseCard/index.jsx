e215e9ef0f56b5cd4847c8ddeaa650c144a1a316
Jean Lima <jcoliveiralima19@gmail.com>
2026-06-03 15:32:40 -0300

    Definindo estrutura básica do card de curso

--- src/Components/CourseCard/index.jsx
+++ src/Components/CourseCard/index.jsx
@@ -1,5 +1,18 @@
-export default function CourseCard() {
+import { Box, Paper, Typography } from "@mui/material";
+
+export default function CourseCard({ image, theme, rating, title, classes, students, price, professor }) {
     return (
-        
+     <Paper>
+        <Box component="img" src={image}></Box>
+        <Box>{theme}</Box>
+        <Box>{rating}</Box>
+        <Typography>{title}</Typography>
+        <Box>{classes}</Box>
+        <Box>{students}</Box>
+        <Box>
+            <Typography>{price}</Typography>
+            <Typography>{professor}</Typography>
+        </Box>
+     </Paper>   
     )
 }
