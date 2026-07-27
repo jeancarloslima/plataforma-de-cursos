52873ff31db2404d0fb7af8d097d2b9df39af5e8
Jean Lima <jcoliveiralima19@gmail.com>
2026-07-27 15:08:23 -0300

    Alterando estrutura básica da página de Cursos Populares

--- src/Components/PopularCoursesLandingPage/index.jsx
+++ src/Components/PopularCoursesLandingPage/index.jsx
@@ -1,20 +1,20 @@
-import { List, ListItem, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import CourseCard from "../CourseCard";
 
 export default function PopularCoursesLandingPage() {
   return (
-    <>
+    <Box>
       <Typography variant="h5">Nossos cursos populares</Typography>
       <Typography variant="body1">
         Descubra nossos cursos mais procurados, curados de forma cuidadosa para
         atender as demandas do mercado atual. Mergulhe em conteúdos construídos
         para o sucesso em cada etapa da sua jornada de estudos.
       </Typography>
-      <List>
-        <ListItem>
-          <CourseCard />
-        </ListItem>
-      </List>
-    </>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CourseCard theme="Web Design" rating={4.9} title="Web Design & Development" classes={25} students={185} price={560} professor="J. Morgan"></CourseCard>
+        <CourseCard theme="Web Design" rating={4.9} title="Web Design & Development" classes={25} students={185} price={560} professor="J. Morgan"></CourseCard>
+        <CourseCard theme="Web Design" rating={4.9} title="Web Design & Development" classes={25} students={185} price={560} professor="J. Morgan"></CourseCard>
+      </Box>
+    </Box>
   );
 }
